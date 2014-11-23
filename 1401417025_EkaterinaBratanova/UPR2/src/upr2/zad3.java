6ed8539b4d54f94279fcacdc39b41f0bb75af69b
fmi <fmi@fmi-535-1.fmi.lcl>
2014-11-23 14:44:00 +0200

    first commit

---
+++ 1401417025_EkaterinaBratanova/UPR2/src/upr2/zad3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package upr2;
+
+/**
+ *
+ * @author fmi
+ */
+public class zad3 {
+    public static void main(String[] args) {
+
+    int i;
+
+    for(i=1; i<=5; i++)
+
+    System.out.println('а');
+
+     for(i=1; i<=10; i++)
+
+    System.out.println('б');
+
+}
+}
---
+++ 1401417025_EkaterinaBratanova/UPR2/src/zad4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fmi
+ */
+public class zad4 {
+    public static void max1(int i1, int i2) {
+
+    int max = i1 > i2 ? i1 : i2;
+
+    System.out.println(max);
+
+}
+
+public static void main(String[] args) {
+
+max1(5, 11);
+
+max1(4, 8);
+    
+}
+}
